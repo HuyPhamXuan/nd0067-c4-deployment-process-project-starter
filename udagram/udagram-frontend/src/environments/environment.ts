a17bec8438ad4b341ec6c82d623b137d53c1d296
HuyPhamXuan <83574475+HuyPhamXuan@users.noreply.github.com>
2023-12-12 13:15:33 +0700

    Update environment.ts

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'huypx2-udagram-api-dev.us-east-1.elasticbeanstalk.com'
+  apiHost: 'http://huypx2-udagram-api-dev.us-east-1.elasticbeanstalk.com/api/v0/'
 };
 
 /*
